a6d286b170073a7bc60d4335af3a4d9df05dc9c3
thach895 <ngo67775@gmail.com>
2026-04-23 19:53:59 +0700

    Add files via upload

---
+++ Bài 1.sql
@@ -0,0 +1,20 @@
+create database productsManager;
+use productsManager;
+
+CREATE TABLE PRODUCTS (
+ProductID INT PRIMARY KEY,
+ProductName VARCHAR(100),
+Category VARCHAR(50),
+OriginalPrice DECIMAL(18, 2)
+);
+ INSERT INTO PRODUCTS (ProductID, ProductName, Category,
+OriginalPrice)
+VALUES
+(1, 'iPhone 15', 'Electronics', 20000000),
+(2, 'Samsung Refrigerator', 'Electronics', 15000000),
+(3, 'Water Spinach', 'Food', 10000),
+(4, 'Filtered Fresh Milk 4', 'Food', 28000);
+
+update PRODUCTS
+set OriginalPrice=OriginalPrice*0.9
+where Category='Electronics'
