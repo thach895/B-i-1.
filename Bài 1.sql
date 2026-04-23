de1ff5ef1ee14eaea53c77a68df2cff12a7bf039
thach895 <ngo67775@gmail.com>
2026-04-23 19:53:44 +0700

    Delete Bài 1.sql

--- Bài 1.sql
+++
@@ -1,9 +0,0 @@
-create database productsManeger;
-use productsManeger;
-
-CREATE TABLE Products (
-	ID INT PRIMARY KEY,
-    productName VARCHAR(255),
-    price DECIMAL(18,2),
-    description TEXT
-);
